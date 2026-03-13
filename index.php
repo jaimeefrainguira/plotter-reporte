09c6204cc95c6ff6bb1aad581727a5237272c52c
Jaime Efrain Guiracocha <45527951+jaimeefrainguira@users.noreply.github.com>
2026-03-12 22:02:13 -0500

    Remove session handling and strict types
    Removed session handling and strict types declaration.

--- index.php
+++ index.php
@@ -3,14 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-declare(strict_types=1);
 
-session_set_cookie_params([
-    'httponly' => true,
-    'samesite' => 'Lax',
-    'secure' => !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off',
-]);
-session_start();
 
 header('X-Content-Type-Options: nosniff');
 header('X-Frame-Options: SAMEORIGIN');
