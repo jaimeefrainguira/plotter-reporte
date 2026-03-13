a85419fa641b5e2295a78825fad2a5a0b6eb4b9c
Jaime Efrain Guiracocha <45527951+jaimeefrainguira@users.noreply.github.com>
2026-03-12 19:57:49 -0500

    Delete js directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.form-delete').forEach((form) => {
-    form.addEventListener('submit', (event) => {
-        if (!confirm('¿Deseas eliminar este reporte? Esta acción no se puede deshacer.')) {
-            event.preventDefault();
-        }
-    });
-});
