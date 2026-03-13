b6641ac90004b7d5bd5412a7c5b1b45f8ff11f0e
Jaime Efrain Guiracocha <45527951+jaimeefrainguira@users.noreply.github.com>
2026-03-13 00:07:22 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -21,6 +21,23 @@ class Database
 
     public function getConnection(): PDO
     {
-        
+        $dsn = sprintf(
+            'mysql:host=%s;dbname=%s;charset=%s',
+            $this->host,
+            $this->dbName,
+            $this->charset
+        );
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            return new PDO($dsn, $this->username, $this->password, $options);
+        } catch (PDOException $exception) {
+            throw new RuntimeException('No fue posible conectar con la base de datos.', 0, $exception);
+        }
     }
 }
