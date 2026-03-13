87331b5da46553afefd6eb4541233910e78e709a
Jaime Efrain Guiracocha <45527951+jaimeefrainguira@users.noreply.github.com>
2026-03-12 22:00:52 -0500

    Fix formatting in database.php

--- config/database.php
+++ config/database.php
@@ -17,6 +17,7 @@ class Database
         $this->username = (string) (getenv('DB_USER') ?: 'mseet_41369034');
         $this->password = (string) (getenv('DB_PASS') ?: '4016508a8b');
         $this->charset = (string) (getenv('DB_CHARSET') ?: 'utf8mb4');
+        
     }
 
     public function getConnection(): PDO
