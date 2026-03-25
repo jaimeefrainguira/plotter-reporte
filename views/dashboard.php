ee8786c4b5c6bdf1d04d6598ba59556cb5a13872
Jaime Guiracocha <jaimeefrainguira@gmail.com>
2026-03-25 14:33:48 -0500

    adf

--- views/dashboard.php
+++ views/dashboard.php
@@ -12,7 +12,6 @@ unset($_SESSION['flash']);
     <title>Dashboard | Reportes Plotter</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="public/css/styles.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
